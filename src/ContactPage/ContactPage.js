03d8c68a4a5a06864d34aee53a02515216407fca
Rosalind H <rosalind.hung99@gmail.com>
2020-07-16 22:42:52 -0700

    simple contact form set up

--- src/ContactPage/ContactPage.js
+++ src/ContactPage/ContactPage.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-
 import './ContactPage.css';
 
 class ContactPage extends Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
 
   render(){ 
+    const { status } = this.state;
+
     return(
         <div className="contact-page">
             <h4>Drop Us a Line</h4>
-      <form className='ContactForm'>
+        <form className='ContactForm' method='post' action='https://formspree.io/xjvapjpd' onSubmit={this.submitForm}>
         <div className='name'>
           <label htmlFor='RegistrationForm_name'>
             Name
@@ -49,7 +76,8 @@ class ContactPage extends Component {
             id='RegistrationForm_message'>
 	  </textarea>
         </div>
-        <input type='submit' value='Send'/>
+        {status === "SUCCESS" ? <p>Thanks! Your email has been sent, talk to you soon!</p> : <input type='submit' value='Send'/>}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
       </form>
         </div>
     );
